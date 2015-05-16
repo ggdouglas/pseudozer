7177ce11be5baa5c39273c1566de3e169c9468fb
Greg Douglas <gwshark0@gmail.com>
2015-05-16 03:12:20 -0400

    Bring things up to snuff

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 var psuedoMap = require('./psuedoMap');
-var seedrandom = require('seedrandom');
+var _ = require('lodash');
 
-var PUNCTUATION_REGEX = /[\x21-\x2F|\x3A-\x40|\x5B-\x60|\x7B-\x7E]/; // ASCII punctuation code ranges
+var BLACKLIST_REGEX = /[\s|\x21-\x2F|\x3A-\x40|\x5B-\x60|\x7B-\x7E]/g; // ASCII punctuation code ranges
+
 var HTML_TAG_REGEX = /<[a-z][\s\S]*>/g;
-var WHITESPACE_REGEX = /\s/;
 
 // checks to see if a string has at least one HTML tag
 function containsHTMLTag(str) {
-	return HTML_TAG_REGEX.test(str)
-}
-
-function containsWhitespace(str) {
-	return WHITESPACE_REGEX.test(str);
-}
-
-function containsPunctuation(str) {
-	return PUNCTUATION_REGEX.test(str);
-}
-
-function getLength(str) {
-	return str.replace(WHITESPACE_REGEX, '').replace(PUNCTUATION_REGEX, '').length;
+	return HTML_TAG_REGEX.test(str);
 }
 
 // translates a string to psuedo-lang
@@ -36,33 +24,72 @@ function surround(str, prefix, suffix) {
 	return str.replace(/(.*)/, prefix + '$1' + suffix);
 }
 
+// gets an even distribution of repeated characters
+// [provide example here]
+function getDistribution(nExtensions, length) {
+	// fill with initial distribution
+	var d0 = _.fill(new Array(length), Math.floor(nExtensions / length));
+
+	// remainder to fill via partition
+	var i = nExtensions % length;
+
+	// split distribution into two partitions
+	var d1 = _(d0.slice(0,i))
+		.map(_.curry(_.add)(1)) // +1 to all elements of first partition
+		.value();
+	var d2 = d0.slice(i);
+
+	// recombine partitions
+	return d1.concat(d2);
+}
+
+function repeatChar(char, n) {
+	return new Array(n + 2).join(char);
+}
+
 function extend(str, percent) {
-	// length sans whitespace and punctuation
-	var length = getLength(str);
-
-	if (length < 1) {
-		return str;
-	}
-
-	var rng = seedrandom(length);
-
-	// number of characters to duplicate
-	var duplicateN = Math.round(length * (1 + percent)) - length;
-
+	var blacklisted = getBlacklisted(str);
 	var chars = str.split('');
 
-	while (duplicateN > 0) {
-		var randomIndex = Math.floor(rng() * chars.length);
-		var char = chars[randomIndex];
+	_.pullAt(chars, _(blacklisted).unzip().first());
 
-		// don't duplicate whitespace or punctuation
-		if (containsWhitespace(char) || containsPunctuation(char)) {
-			continue;
-		}
+	// do stuff with chars
 
-		chars.splice(randomIndex, 0, char);
-		duplicateN--;
-	}
+	var length = chars.length;
+	var nExtensions = Math.round(length * percent);
 
-	return chars.join('');
+	var distribution = getDistribution(nExtensions, length);
+
+	// apply repeat distribution to chars
+	chars = _.zipWith(chars, distribution, function(char, n) {
+		return repeatChar(char, n);
+	});
+
+	return insertBlacklisted(chars, blacklisted).join('');
 }
+
+function getBlacklisted(str) {
+	var blacklisted = [];
+	while(BLACKLIST_REGEX.test(str)) {
+		var i = (BLACKLIST_REGEX.lastIndex - 1);
+		blacklisted.push([i, str[i]]);
+	}
+	return blacklisted;
+}
+
+function insertBlacklisted(chars, blacklisted) {
+	_(blacklisted)
+		.forEach(_.spread(function(i, char) {
+			chars.splice(i, 0, char);
+		}))
+		.value();
+
+	return chars;
+}
+
+var str = 'abc!';
+
+var x = extend(str, 0.5);
+
+// HOLY SHIT, IT WORKS!!!!
+console.log(x);
